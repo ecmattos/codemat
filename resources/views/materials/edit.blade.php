8698b22aab217526c3784f213e3b29100aa34e6e
Eduardo C de Mattos <ecmattos@ig.com.br>
2015-07-04 02:02:07 -0300

    Material - Form edit

--- resources/views/materials/edit.blade.php
+++ resources/views/materials/edit.blade.php
@@ -4,6 +4,7 @@
 	<div class="container"> 
 		<h3>Materiais - Alteração</h3>
 		<a href="{{ route('materials') }}" class="btn btn-default"><i class='glyphicon glyphicon-list'></i></a>
+		
 		{!! Form::model($material, ['route' => ['materials.update', $material->id], 'method' => 'put', 'class' => 'form-horizontal', 'role'=>'form']) !!}
 			
 			@include('materials.partials.form')
