318b630fd4acef8a3697239042d48e3fe44f6b48
Eduardo C de Mattos <ecmattos@ig.com.br>
2015-07-04 01:50:21 -0300

    Material Group - Form edit

--- resources/views/material_groups/edit.blade.php
+++ resources/views/material_groups/edit.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 	<div class="container"> 
-		<h3>Materiais: situações - Alteração</h3>
+		<h3>Materiais: grupos - Alteração</h3>
 		<a href="{{ route('material_groups') }}" class="btn btn-default"><i class='glyphicon glyphicon-list'></i></a>
 
-		{!! Form::open(array('route' => array('material_groups.update', $material_group->id), 'method' => 'put', 'class' => 'form-horizontal', 'role'=>'form')) !!}
-			
+		{!! Form::model($material_group, ['route' => ['material_groups.update', $material_group->id], 'method' => 'put', 'class' => 'form-horizontal', 'role'=>'form']) !!}
+
 			@include('material_groups.partials.form')
 			
 		{!! Form::close() !!}
